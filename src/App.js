9efb3982255d950fb00b2fc37135a76b904095f2
Your Name <mahbub1122019@gmail.comm>
2020-08-27 11:22:56 -0700

    educational website done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import Course from './components/Course/Course';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Course></Course>
+    </div>
+  );
+}
+
+export default App;
